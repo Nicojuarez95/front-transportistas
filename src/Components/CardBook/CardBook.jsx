cb5da950b7c3bdd5d29896c0ddbc092ed3df0b57
Nicolas Juarez <juareznico61@gmail.com>
2024-08-26 10:12:32 -0300

    web transportistas

--- src/Components/CardBook/CardBook.jsx
+++ src/Components/CardBook/CardBook.jsx
@@ -11,9 +11,10 @@ export default function CardBook({ id, estado, vehiculo, cliente, fecha, pago, t
     setShowConfirmation(true);
   };
 
+
+
+
   
-
-
   const confirmDelete = () => {
     LoadStart()
     axios.delete(`http://localhost:8080/book/delete/${id}`)
